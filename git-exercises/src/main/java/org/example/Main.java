539f19e757262038539517084e62ed45dffcbc0e
TobiOneCanobi <vlogkongen@gmail.com>
2024-02-01 11:21:42 +0100

    Tobi's push
    yes sir (^w^)7

--- git-exercises/src/main/java/org/example/Main.java
+++ git-exercises/src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printNumbers();
     }
+
+    public static void printNumbers() {
+        int numberMax = 101;
+
+        for (int i = 0; i < numberMax; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else
+                System.out.println(i);
+        }
+    }
+
 }
+
+
